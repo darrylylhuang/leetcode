7f65b63d6dfa7b481c70cb16d5519db54a86c975
darryI <darryl.thatguy.13@gmail.com>
2024-10-07 17:08:16 -0400

    Starter code.

---
+++ 271.py
@@ -0,0 +1,17 @@
+# Encode and Decode Strings
+class Solution:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        return
+        # write your code here
+
+    """
+    @param: str: A string
+    @return: decodes a single string to a list of strings
+    """
+    def decode(self, str):
+        return
+        # write your code here
