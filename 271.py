8cfed5592d2d2b977043cef9e095295ba323b2f1
darryI <darryl.thatguy.13@gmail.com>
2024-10-07 17:34:50 -0400

    Attempt to resolve empty strings and empty lists (Incomplete).

--- 271.py
+++ 271.py
@@ -5,17 +5,17 @@ class Solution:
         @param: strs: a list of strings
         @return: encodes a list of strings to a single string.
         """
-        encoded = ""
+        encoded = str(len(strs))
         for i in range(len(strs)):
-            if i == len(strs) - 1:
-                encoded += strs[i]
-            else:
-                encoded += strs[i] + "\0"
-        return encoded.strip()
+            encoded += strs[i] + "\0"
+        return encoded
 
     def decode(self, str):
         """
         @param: str: A string
         @return: decodes a single string to a list of strings
         """
-        return str.split("\0")
+        decoded = str.split("\0")
+        if decoded[0] > len(decoded) - 1:
+            return decoded[1:-1]
+        return decoded[1:]
