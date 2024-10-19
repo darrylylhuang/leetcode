ce78b9c163a20be40155b65692bc4d9e89f729fe
darryI <darryl.thatguy.13@gmail.com>
2024-10-19 18:35:23 -0400

    Solved the problem by using prefixes as placeholders and tracking postfixes as an int.

--- 238.py
+++ 238.py
@@ -4,3 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        products = [nums[0]]
+        # prefix products
+        # placeholders in solution space
+        for i in range(1, len(nums)):
+            products.append(nums[i] * products[i - 1])
+        # postfix products
+        # tracked as int
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            prefix = products[i-1]
+            if i == 0:
+                prefix = 1
+            products[i] = prefix * postfix
+            postfix *= nums[i]
+        return products
