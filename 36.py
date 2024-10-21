811b3609cbc5910ab1e74aff778548d7a81733f8
darryI <darryl.thatguy.13@gmail.com>
2024-10-21 15:07:10 -0400

    Loops through all elements and checks rows for duplicates.

--- 36.py
+++ 36.py
@@ -4,4 +4,10 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        
+        for row in board:
+            current_row = set()
+            for box in row:
+                if box != "." and box in current_row:
+                    return False
+                else:
+                    current_row.add(box)
