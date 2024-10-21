29a72ea0f7cc7ecdb3bb62e1b38e35b10823a114
darryI <darryl.thatguy.13@gmail.com>
2024-10-21 16:28:50 -0400

    Added logic for `mini_squares`.

--- 36.py
+++ 36.py
@@ -5,6 +5,7 @@ class Solution(object):
         :rtype: bool
         """
         columns = {x: set() for x in range(9)}
+        mini_squares = {x: set() for x in range(9)}
 
         for i in range(len(board)):
             row = board[i]
@@ -27,6 +28,14 @@ class Solution(object):
                     return False
                 else:
                     columns[j].add(box)
+
+                # map indices to the range of [0, 2]
+                # basically, we simulate a 3x3 as if it were represented by a single list
+                k = (i // 3) * 3 + (j // 3)
+                if box in mini_squares[k]:
+                    return False
+                else:
+                    mini_squares[k].add(box)
 
         # default case
         return True
