9023ddae60bb835297c2092b47d13aeb2370a905
darryI <darryl.thatguy.13@gmail.com>
2024-10-22 22:33:41 -0400

    Find sequences and keep track of the longest.

--- 128.py
+++ 128.py
@@ -4,33 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # con_lens stands for consecutive lengths
-        con_lens = dict()
-        for num in nums:
-            # don't need to do anything if a num already exists
-            if num not in con_lens:
-                # new num - four cases:
-                # 3. num - 1 exists AND num + 1 exists
-                if num - 1 in con_lens and num + 1 in con_lens:
-                    con_lens[num] = con_lens[num - 1] + con_lens[num + 1]
-                    con_lens[num] += 1
-                    con_lens[num - 1] = con_lens[num]
-                    con_lens[num + 1] = con_lens[num]
-                # 1. only num - 1 exists
-                elif num - 1 in con_lens:
-                    con_lens[num - 1] += 1
-                    con_lens[num] = con_lens[num - 1]
-                # 2. only num - 1 exists
-                elif num + 1 in con_lens:
-                    con_lens[num + 1] += 1
-                    con_lens[num] = con_lens[num + 1]
-                # 4. new num is an island
-                else:
-                    con_lens[num] = 1
-
         longest = 0
-        for length in con_lens.values():
-            if length > longest:
-                longest = length
+        nums_set = set(nums)
+        for num in nums:
+            # this is the beginning of a sequence
+            if num - 1 not in nums_set:
+                curr = 1
+                while (num + curr in nums_set):
+                    curr += 1
+                longest = max(curr, longest)
 
         return longest
