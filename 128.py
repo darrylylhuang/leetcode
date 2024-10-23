13e96eddc9bcfc4d1fde583d61f1020a4de87f2c
darryI <darryl.thatguy.13@gmail.com>
2024-10-22 21:29:59 -0400

    Initial idea, but it does not stitch adjacent sets together.

--- 128.py
+++ 128.py
@@ -4,4 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        all_sets = dict()
+        for num in nums:
+            if num in all_sets:
+                all_sets[num].add(num)
+                all_sets[num + 1] = all_sets[num]
+                all_sets[num - 1] = all_sets[num]
+            else:
+                new_set = set()
+                new_set.add(num)
+                all_sets[num] = new_set
+                all_sets[num + 1] = new_set
+                all_sets[num - 1] = new_set
+
+        largest_set = 0
+        for s in all_sets.values():
+            if len(s) > largest_set:
+                largest_set = len(s)
+
+        return largest_set
