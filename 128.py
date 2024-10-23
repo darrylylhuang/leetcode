3b47257f344eeb641d4355b61cf3dd018f82514b
darryI <darryl.thatguy.13@gmail.com>
2024-10-22 21:55:51 -0400

    Second attempt, but it does not actually correct the chain.

--- 128.py
+++ 128.py
@@ -7,12 +7,21 @@ class Solution(object):
         all_sets = dict()
         for num in nums:
             if num in all_sets:
-                all_sets[num].add(num)
-                all_sets[num + 1] = all_sets[num]
+                # num exists - three cases:
+                # 3. num - 1 exists AND num + 1 exists
+                if num - 1 in all_sets and num + 1 in all_sets:
+                    all_sets[num] = all_sets[num - 1].union(all_sets[num + 1])
+                    all_sets[num].add(num)
+                    # update the other two sets
+                # cases 1 and 2, num was added by one of its neighbours
+                else:
+                    all_sets[num].add(num)
+                # in all cases, update the surrounding sets
                 all_sets[num - 1] = all_sets[num]
+                all_sets[num + 1] = all_sets[num]
             else:
-                new_set = set()
-                new_set.add(num)
+                # num DNE which means adjacent numbers do not exist
+                new_set = {num}
                 all_sets[num] = new_set
                 all_sets[num + 1] = new_set
                 all_sets[num - 1] = new_set
