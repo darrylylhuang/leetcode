647c0d2e06be46247e6b3f678e1b8bac8c5bc7c8
darryI <darryl.thatguy.13@gmail.com>
2024-10-25 11:31:23 -0400

    Loop through characters on both ends to find alphanums and compare

--- 125.py
+++ 125.py
@@ -4,7 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        for i in range(len(s)):
-            if s[i] != s[len(s) - 1 - i]:
+        i = 0
+        j = len(s) - 1
+        while i < len(s) and j >= 0:
+            # search right from start for alphanum
+            while not s[i].isalnum() and i < len(s):
+                i += 1
+            # search left from end for alphanum
+            while not s[j].isalnum() and j >= 0:
+                j -= 1
+            # two alphanums found; compare
+            if s[i] != s[j]:
                 return False
+            i += 1
+            j -= 1
         return True
