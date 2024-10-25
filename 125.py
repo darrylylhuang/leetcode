1b58888a16e899c0bd422f42469d486bdaf807b2
darryI <darryl.thatguy.13@gmail.com>
2024-10-25 11:51:28 -0400

    Fixed loop indices (string must stay in bounds during the outer while loop)

--- 125.py
+++ 125.py
@@ -6,19 +6,13 @@ class Solution(object):
         """
         i = 0
         j = len(s) - 1
-        while True:
+        while i < len(s) and j > -1:
             # search right from start for alphanum
-            while i < len(s) and not s[i].isalnum():
+            while i < len(s) - 1 and not s[i].isalnum():
                 i += 1
             # search left from end for alphanum
-            while j >= 0 and not s[j].isalnum():
+            while j > 0 and not s[j].isalnum():
                 j -= 1
-
-            # stop the loop if we've gone out of bounds
-            if i >= len(s):
-                break
-            if j < 0:
-                break
 
             # two alphanums found; compare
             if s[i] != s[j]:
