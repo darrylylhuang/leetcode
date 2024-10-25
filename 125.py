4174bde366ba22137d6937ebbdcd3e1f72b29946
darryI <darryl.thatguy.13@gmail.com>
2024-10-25 11:35:50 -0400

    Idea on how to maintain loop integrity, but there's probably a better solution

--- 125.py
+++ 125.py
@@ -6,13 +6,20 @@ class Solution(object):
         """
         i = 0
         j = len(s) - 1
-        while i < len(s) and j >= 0:
+        while True:
             # search right from start for alphanum
-            while not s[i].isalnum() and i < len(s):
+            while i < len(s) and not s[i].isalnum():
                 i += 1
             # search left from end for alphanum
-            while not s[j].isalnum() and j >= 0:
+            while j >= 0 and not s[j].isalnum():
                 j -= 1
+
+            # stop the loop if we've gone out of bounds
+            if i >= len(s):
+                break
+            if j < 0:
+                break
+
             # two alphanums found; compare
             if s[i] != s[j]:
                 return False
