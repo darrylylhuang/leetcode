b8f11977b461ba08c40503a5f4b5c647a52787df
darryI <darryl.thatguy.13@gmail.com>
2024-10-25 12:11:42 -0400

    Went back to original idea and correctly managed indices

--- 125.py
+++ 125.py
@@ -6,13 +6,18 @@ class Solution(object):
         """
         i = 0
         j = len(s) - 1
-        while i < len(s) and j > -1:
+        while True:
             # search right from start for alphanum
-            while i < len(s) - 1 and not s[i].isalnum():
+            while i < len(s) and not s[i].isalnum():
                 i += 1
+            if i == len(s):
+                break
+
             # search left from end for alphanum
-            while j > 0 and not s[j].isalnum():
+            while j > -1 and not s[j].isalnum():
                 j -= 1
+            if j == -1:
+                break
 
             # two alphanums found; compare
             if s[i].lower() != s[j].lower():
