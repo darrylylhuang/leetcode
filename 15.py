a0c58d1f1662bf65545f8db23bb1a56444751fd3
darryI <darryl.thatguy.13@gmail.com>
2024-10-26 19:15:02 -0400

    Solution using `twoSum`. It overcompensates.

--- 15.py
+++ 15.py
@@ -4,3 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        solution = []
+        for i in range(len(nums)):
+            twoSum = self.twoSum(nums, -1 * nums[i])
+            if twoSum != []:
+                solution.append([nums[i], nums[twoSum[0]], nums[twoSum[1]]])
+        return solution
+
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        differences = {}
+        for i in range(len(nums)):
+            if target - nums[i] in differences:
+                return [differences[target - nums[i]], i]
+            differences[nums[i]] = i
+        return []
