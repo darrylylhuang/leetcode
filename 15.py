809dcdf771bfb918ac247e5e4c6ee9da687650d6
darryI <darryl.thatguy.13@gmail.com>
2024-10-28 16:55:40 -0400

    Normalizing to 0 gives us independent, and exhaustive cases.

--- 15.py
+++ 15.py
@@ -9,20 +9,22 @@ class Solution(object):
         # fix a number -- nums[i]
         i = 0
         while i < len(nums):
-            # do a twoSum solution on the rest of the list
+            # fix a number--nums[i]--for "every" element in the list
             j = i + 1
             k = len(nums) - 1
             while j < k:
-                if nums[j] + nums[k] > nums[i]:
+                if nums[i] + nums[j] + nums[k] > 0:
                     k -= 1
-                if nums[j] + nums[k] < nums[i]:
+                elif nums[i] + nums[j] + nums[k] < 0:
                     j += 1
-                if nums[j] + nums[k] == -1 * nums[i]:
+                else:
                     solution.append([nums[i], nums[j], nums[k]])
                     while j < k and nums[j] == nums[j + 1]:
                         j += 1
                     j += 1
+
             # we've exhausted all possibilities with the current fixed number at i
+            # skip duplicates
             while i < len(nums) - 1 and nums[i] == nums[i + 1]:
                 i += 1
             i += 1
