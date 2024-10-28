cff8374ebcf609393309e3030923c0f0f63d6d56
darryI <darryl.thatguy.13@gmail.com>
2024-10-28 16:20:14 -0400

    Skip through duplicate numbers where we've exhausted all cases.

--- 15.py
+++ 15.py
@@ -7,7 +7,8 @@ class Solution(object):
         solution = []
         nums.sort()
         # fix a number -- nums[i]
-        for i in range(len(nums)):
+        i = 0
+        while i < len(nums):
             # do a twoSum solution on the rest of the list
             j = i + 1
             k = len(nums) - 1
@@ -20,3 +21,9 @@ class Solution(object):
                     solution.append([nums[i], nums[j], nums[k]])
                     while j < k and nums[j] == nums[j + 1]:
                         j += 1
+                    j += 1
+            # we've exhausted all possibilities with the current fixed number at i
+            while i < len(nums) - 1 and nums[i] == nums[i + 1]:
+                i += 1
+            i += 1
+        return solution
