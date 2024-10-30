ab801d74b390ec80d19ad72d9907ca5e8402a555
darryI <darryl.thatguy.13@gmail.com>
2024-10-30 13:20:00 -0400

    "Greedy" solution using two pointers.

--- 11.py
+++ 11.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        i = 0
+        j = len(height) - 1
+        max_area = 0
+        while i < j:
+            # length * width where the shorter height determines how much water can be held
+            max_area = max(max_area, min(height[i], height[j]) * (j - i))
+
+            # "eliminate" the shorter of the heights as it is the bottleneck
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return max_area
