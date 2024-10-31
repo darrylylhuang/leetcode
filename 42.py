633c592607ceb6543d656f19e3a8939d8b86418e
darryI <darryl.thatguy.13@gmail.com>
2024-10-31 13:15:50 -0400

    Adds trapped rainwater for "small valleys" by utilizing the stack.

--- 42.py
+++ 42.py
@@ -21,12 +21,18 @@ class Solution(object):
         # TODO: outer loop to repeat "large valley" procedure
 
         # start looking for the right barrier of a "large" valley
+        r += 1
         while r < len(height) and height[r] < height[l]:
-            if len(stack) == 0 or height[r] < stack[-1]:
-                stack.append()
+            if len(stack) == 0 or height[r] < stack[-1][1]:
+                stack.append((r, height[r]))
+            if height[r] > stack[-1][1]:
+                trapped += (r - stack[-1][0]) * (height[r] - stack[-1][1])
+                stack.pop()
             r += 1
 
         # large valley found
+        # stack is not relevant to large valleys
+        stack = []
         left_barrier = height[l]
         l += 1
         while l < r:
