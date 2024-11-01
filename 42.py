12830d0c2dfd63657d27f21273ee0aa12e23baac
darryI <darryl.thatguy.13@gmail.com>
2024-11-01 17:36:41 -0400

    Added some logic for intermediate left barriers, but may need more cases.

--- 42.py
+++ 42.py
@@ -24,12 +24,24 @@ class Solution(object):
             if len(stack) == 0 or height[r] < stack[-1][1]:
                 stack.append((r, height[r]))
 
-            # new barrier found
+            # new right barrier found
             if height[r] > stack[-1][1]:
                 (width, length) = stack.pop()
-                trapped += (r - width) * (min(height[l], height[r]) - length)
-                # if left barrier is the bottleneck, we've added all rain possible from that side
-                if height[l] < height[r]:
+                # stack is not empty, so we have an intermediate left barrier
+                if len(stack) != 0:
+                    left_bar = stack[-1][1]
+                else:
+                    left_bar = height[l]
+
+                # TODO: May need two cases here:
+                # 1. Stack empty = left barrier
+                # 2. Stack not empty = intermediate left barrier
+
+                if height[r] < left_bar:
+                    trapped += (r - width) * (height[r] - length)
+                else:
+                    trapped += (r - width) * (left_bar - length)
+                    # if left barrier is the bottleneck, we've added all rain possible from that side
                     l = r
             r += 1
 
