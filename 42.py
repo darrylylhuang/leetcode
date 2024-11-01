13016dd00c08471ae79f3ebd96833879f9192e3b
darryI <darryl.thatguy.13@gmail.com>
2024-11-01 17:03:53 -0400

    Algorithm works with 1st example test case

--- 42.py
+++ 42.py
@@ -17,28 +17,20 @@ class Solution(object):
         # 2. height[0] != 0 : l == 0 == r
         l = r
 
-        # TODO: find smaller valleys
-        # TODO: outer loop to repeat "large valley" procedure
-
-        # start looking for the right barrier of a "large" valley
+        # start looking for more barriers
         r += 1
-        while r < len(height) and height[r] < height[l]:
+        while r < len(height):
+            # new trough found
             if len(stack) == 0 or height[r] < stack[-1][1]:
                 stack.append((r, height[r]))
+
+            # new barrier found
             if height[r] > stack[-1][1]:
-                trapped += (r - stack[-1][0]) * (height[r] - stack[-1][1])
-                stack.pop()
+                (width, length) = stack.pop()
+                trapped += (r - width) * (min(height[l], height[r]) - length)
+                # if left barrier is the bottleneck, we've added all rain possible from that side
+                if height[l] < height[r]:
+                    l = r
             r += 1
 
-        # large valley found
-        # stack is not relevant to large valleys
-        stack = []
-        left_barrier = height[l]
-        l += 1
-        while l < r:
-            trapped += left_barrier - height[l]
-            l += 1
-
-        # we've reached the end of the list
-        if l == len(height):
-            return trapped
+        return trapped
