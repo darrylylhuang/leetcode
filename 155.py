429f21834d809d872c09c82c4cdcf6503c143369
darryI <darryl.thatguy.13@gmail.com>
2024-11-04 11:46:26 -0500

    Removed comments

--- 155.py
+++ 155.py
@@ -32,11 +32,3 @@ class MinStack(object):
         :rtype: int
         """
         return self._mins[-1]
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
