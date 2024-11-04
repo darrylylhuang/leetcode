fbc2162ca90cd108c8eebe835b7b6884042d3e07
darryI <darryl.thatguy.13@gmail.com>
2024-11-03 19:17:30 -0500

    Solution

--- 20.py
+++ 20.py
@@ -4,3 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        stack = []
+        bracket_map = {"(": ")", "[": "]", "{": "}"}
+        for char in s:
+            # add open brackets to the stack
+            if char in bracket_map:
+                stack.append(char)
+            # when seeing a close bracket, pop off the stack and ensure validity
+            else:
+                # unopened bracket
+                if len(stack) == 0:
+                    return False
+                # incorrect bracket closing
+                if char != bracket_map[stack.pop()]:
+                    return False
+        # unclosed bracket(s)
+        if len(stack) != 0:
+            return False
+        return True
