59bd5138c7a3bb7fe8f8454738be6d2002bada3b
darryI <darryl.thatguy.13@gmail.com>
2024-11-05 23:10:33 -0500

    Initial solution with debugging (negative numbers don't work)

--- 150.py
+++ 150.py
@@ -4,3 +4,31 @@ class Solution(object):
         :type tokens: List[str]
         :rtype: int
         """
+        stack = []
+        for token in tokens:
+            if token.isnumeric():
+                print("putting" + token + "in the stack")
+                stack.append(int(token))
+            # operator
+            else:
+                # second token is at the top of the stack
+                right = stack.pop()
+                left = stack.pop()
+                print("doing" + token + "on" + str(left) + "and" + str(right))
+                if token == "+":
+                    stack.append(left + right)
+                if token == "-":
+                    stack.append(left - right)
+                if token == "*":
+                    stack.append(left * right)
+                if token == "/":
+                    stack.append(left // right)
+
+        return stack.pop()
+
+
+if __name__ == "__main__":
+    tokens = ["10", "6", "9", "3", "+", "-11",
+              "*", "/", "*", "17", "+", "5", "+"]
+    solution = Solution()
+    print(solution.evalRPN(tokens))
