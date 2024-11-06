6a07b29e08991c03510433b87a7d5852e1dc691b
darryI <darryl.thatguy.13@gmail.com>
2024-11-06 00:14:56 -0500

    Check for the operators first: valid input means `else` case will be a number

--- 150.py
+++ 150.py
@@ -6,44 +6,38 @@ class Solution(object):
         """
         stack = []
         for token in tokens:
-            if token.isdigit():
-                stack.append(int(token))
-            # operator
+            if token == "+":
+                right, left = stack.pop(), stack.pop()
+                stack.append(left + right)
+            elif token == "-":
+                right, left = stack.pop(), stack.pop()
+                stack.append(left - right)
+            elif token == "*":
+                right, left = stack.pop(), stack.pop()
+                stack.append(left * right)
+            elif token == "/":
+                right, left = stack.pop(), stack.pop()
+                # truncate towards 0
+                # case 2: quotient negative non-whole number
+                if (left > 0 and right < 0) or (left < 0 and right > 0):
+                    # ceiling divison
+                    new_token = -(left // -right)
+                # case 1: quotient positive
+                else:
+                    new_token = left // right
+                stack.append(new_token)
+            # number
             else:
-                # case 1: negative number
-                if token.strip("-").isdigit():
-                    stack.append(int(token))
-                    continue
-
-                # case 2: operator
-                # second token is at the top of the stack
-                right = stack.pop()
-                left = stack.pop()
-
-                if token == "+":
-                    stack.append(left + right)
-                if token == "-":
-                    stack.append(left - right)
-                if token == "*":
-                    stack.append(left * right)
-                if token == "/":
-                    # truncate towards 0
-                    # case 2: quotient negative non-whole number
-                    if (left > 0 and right < 0) or (left < 0 and right > 0):
-                        # ceiling divison
-                        new_token = -(left // -right)
-                    # case 1: quotient positive
-                    else:
-                        new_token = left // right
-                    stack.append(new_token)
+                stack.append(int(token))
 
         return stack.pop()
 
 
 if __name__ == "__main__":
     solution = Solution()
-    # tokens = ["10", "6", "9", "3", "+", "-11",
-    #           "*", "/", "*", "17", "+", "5", "+"]
-    # print(solution.evalRPN(tokens))
-    tokens = ["4", "-2", "/", "2", "-3", "-", "-"]
+    tokens = ["10", "6", "9", "3", "+", "-11",
+              "*", "/", "*", "17", "+", "5", "+"]
+
     print(solution.evalRPN(tokens))
+    # tokens = ["4", "-2", "/", "2", "-3", "-", "-"]
+    # print(solution.evalRPN(tokens))
