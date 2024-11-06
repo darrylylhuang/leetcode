a25d7cc07d25dcf5c3514d86f23cc7c0000adaed
darryI <darryl.thatguy.13@gmail.com>
2024-11-05 23:46:30 -0500

    Truncate towards 0 correctly (ceiling divsion when ONE of the operands is negative); also token is already a negative number when parsed.

--- 150.py
+++ 150.py
@@ -7,19 +7,19 @@ class Solution(object):
         stack = []
         for token in tokens:
             if token.isnumeric():
-                print("putting" + token + "in the stack")
                 stack.append(int(token))
             # operator
             else:
                 # case 1: negative number
                 if token.strip("-").isnumeric():
-                    stack.append(-1 * int(token))
+                    stack.append(int(token))
                     continue
+
                 # case 2: operator
                 # second token is at the top of the stack
                 right = stack.pop()
                 left = stack.pop()
-                print("doing" + token + "on" + str(left) + "and" + str(right))
+
                 if token == "+":
                     stack.append(left + right)
                 if token == "-":
@@ -27,13 +27,23 @@ class Solution(object):
                 if token == "*":
                     stack.append(left * right)
                 if token == "/":
-                    stack.append(left // right)
+                    # truncate towards 0
+                    # case 2: quotient negative non-whole number
+                    if (left > 0 and right < 0) or (left < 0 and right > 0):
+                        # ceiling divison
+                        new_token = -(left // -right)
+                    # case 1: quotient positive
+                    else:
+                        new_token = left // right
+                    stack.append(new_token)
 
         return stack.pop()
 
 
 if __name__ == "__main__":
-    tokens = ["10", "6", "9", "3", "+", "-11",
-              "*", "/", "*", "17", "+", "5", "+"]
     solution = Solution()
+    # tokens = ["10", "6", "9", "3", "+", "-11",
+    #           "*", "/", "*", "17", "+", "5", "+"]
+    # print(solution.evalRPN(tokens))
+    tokens = ["4", "-2", "/", "2", "-3", "-", "-"]
     print(solution.evalRPN(tokens))
