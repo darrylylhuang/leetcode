ff69c1986a84383dc85839960f09c7d44c617d93
darryI <darryl.thatguy.13@gmail.com>
2024-11-07 18:02:06 -0500

    Attempt to refine open/close logic.

--- 22.py
+++ 22.py
@@ -7,16 +7,20 @@ class Solution(object):
         result = []
         stack = []
 
+        current_combo = "("
+        stack.append(current_combo)
+        n -= 1
         while True:
-            current_combo = ""
             # try open
             if n > 0:
+                current_combo += "("
                 stack.append("(")
                 n -= 1
 
             # try close
-            if "(" in stack:
-                stack.append(")")
+            if stack:
+                stack.pop()
+                current_combo += ")"
 
             if n == 0:
                 break
