ae7228ee802c6c06055bbea546f971a08367d075
darryI <darryl.thatguy.13@gmail.com>
2024-11-06 23:59:37 -0500

    Initial "open"/"close" logic attempts.

--- 22.py
+++ 22.py
@@ -4,3 +4,21 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        result = []
+        stack = []
+
+        while True:
+            current_combo = ""
+            # try open
+            if n > 0:
+                stack.append("(")
+                n -= 1
+
+            # try close
+            if "(" in stack:
+                stack.append(")")
+
+            if n == 0:
+                break
+
+        return result
