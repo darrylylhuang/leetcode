301345c96ba4f8edcc92d09fd5d03eb877730221
darryI <darryl.thatguy.13@gmail.com>
2024-11-08 14:00:53 -0500

    Removed tuples since `temperatures` already contains the info we need

--- 739.py
+++ 739.py
@@ -7,8 +7,8 @@ class Solution(object):
         stack = []
         answer = [0] * len(temperatures)
         for i in range(len(temperatures)):
-            while stack and temperatures[i] > stack[-1][1]:
-                j = stack.pop()[0]
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                j = stack.pop()
                 answer[j] = i - j
-            stack.append((i, temperatures[i]))
+            stack.append(i)
         return answer
