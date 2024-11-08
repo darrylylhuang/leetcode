3284644153d42ef37cebb6b2a2d7a96e0d21a264
darryI <darryl.thatguy.13@gmail.com>
2024-11-08 13:55:05 -0500

    temperature unnecessary when we pop from the stack

--- 739.py
+++ 739.py
@@ -8,7 +8,7 @@ class Solution(object):
         answer = [0] * len(temperatures)
         for i in range(len(temperatures)):
             while stack and temperatures[i] > stack[-1][1]:
-                (j, temp) = stack.pop()
+                j = stack.pop()[0]
                 answer[j] = i - j
             stack.append((i, temperatures[i]))
         return answer
