5d1865991148e3b0b56a6edd7b12d0ce4d653afb
darryI <darryl.thatguy.13@gmail.com>
2024-11-08 13:27:01 -0500

    Initial ideas (realized intermediate "high" temperatures are problematic)

--- 739.py
+++ 739.py
@@ -4,3 +4,13 @@ class Solution(object):
         :type temperatures: List[int]
         :rtype: List[int]
         """
+        stack = []
+        answer = []
+        days = 0
+        for temperature in temperatures:
+            if not stack or temperature < stack[-1]:
+                stack.append(temperature)
+            else:
+                answer.append(len(stack))
+                stack.pop()
+                stack.append(temperature)
