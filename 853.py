7a8596ad7697fba1d1d23da67c57d5b376abb59a
darryI <darryl.thatguy.13@gmail.com>
2024-11-08 16:07:31 -0500

    Can add speeds to positions until all cars reach the target

--- 853.py
+++ 853.py
@@ -6,3 +6,15 @@ class Solution(object):
         :type speed: List[int]
         :rtype: int
         """
+        stack = []
+        new_positions = position
+        while new_positions != [target] * len(position):
+            for i, s in enumerate(speed):
+                new_positions[i] += s
+                if not stack or stack[-1] > new_positions[i]:
+                    stack.append(new_positions[i])
+                else:
+                    # a car has caught up to another
+                    new_positions[i] = stack[-1]
+                    # TODO: identify fleet
+                    # TODO: update speed somehow
