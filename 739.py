c51bb5470bc13fc387b976db083a9c8500b17a94
darryI <darryl.thatguy.13@gmail.com>
2024-11-08 13:50:26 -0500

    Prefilling the `answer` allows for `0`s and to use list index instead of `insert()`

--- 739.py
+++ 739.py
@@ -5,13 +5,13 @@ class Solution(object):
         :rtype: List[int]
         """
         stack = []
-        answer = []
+        answer = [0] * len(temperatures)
         for i in range(len(temperatures)):
             if not stack or temperatures[i] <= stack[-1][1]:
                 stack.append((i, temperatures[i]))
             else:
                 while stack and temperatures[i] > stack[-1][1]:
                     (j, temp) = stack.pop()
-                    answer.insert(j, i - j)
+                    answer[j] = i - j
                 stack.append((i, temperatures[i]))
         return answer
