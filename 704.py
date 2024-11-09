68a91051442d37c1fbbe33f023f4e25815a26f51
darryI <darrylylhuang@gmail.com>
2024-11-09 16:55:20 -0500

    Added offset

--- 704.py
+++ 704.py
@@ -5,20 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        offset = 0
         # list size cannot go below 1 when slicing
         while len(nums) > 1:
             # because the list is sorted, we'll begin checking at the middle
             mid = len(nums) // 2
             if nums[mid] == target:
-                return mid
+                return mid + offset
             # check the lesser half of the list
             elif nums[mid] > target:
                 nums = nums[:mid]
             # check the greater half of the list
             else:
+                offset += mid
                 nums = nums[mid:]
 
         # empty list means target never found
         if nums[0] == target:
-            return 0
+            return offset
         return -1
