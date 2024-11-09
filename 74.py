9f290cbd6360b47c1f48cd886d0ff86b531df3ee
darryI <darrylylhuang@gmail.com>
2024-11-09 17:36:16 -0500

    Initial solution

--- 74.py
+++ 74.py
@@ -5,3 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
+        m = len(matrix)
+        n = len(matrix[0])
+        mid = m * n // 2
+        offset = 0
+        while mid > 0:
+            i = mid + offset // n
+            j = mid % n
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                mid = mid // 2
+            else:
+                mid = mid // 2
+                offset += mid
+        
+        i = offset // n
+        j = offset % n
+        # final element
+        return matrix[i][j] == target
