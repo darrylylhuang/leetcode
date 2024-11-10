2f98a87a432ade371006fb485eabd6450078b98b
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-10 14:17:08 -0500

    `O(nlogn)` solution

--- 875.py
+++ 875.py
@@ -8,4 +8,19 @@ class Solution(object):
         # if we have a very generous h, we can eat one banana at a time
         # having a banana-eating rate higher than our largest pile is pointless
         l, r = 1, max(piles)
-        mid = (l + r) // 2
+        # longest_time = 0
+        lowest_eating_rate = r
+        while l < r:
+            time_taken = 0
+            mid = (l + r) // 2
+            for pile in piles:
+                # ceiling divison
+                time_taken += -(pile // -mid)
+
+            if time_taken > h:
+                l = mid + 1
+            else:
+                r = mid - 1
+                # longest_time = time_taken
+                lowest_eating_rate = mid
+        return lowest_eating_rate
