374a4e402a97bf769ecf474a025aa4851051873f
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-10 17:00:32 -0500

    Shift pointers correctly based on sorting

--- 153.py
+++ 153.py
@@ -5,11 +5,21 @@ class Solution(object):
         :rtype: int
         """
         l, r = 0, len(nums) - 1
-        while l <= r:
-             mid = (l + r) // 2
-             if nums[mid + 1] < nums[mid]:
-                 return nums[mid + 1]
-             elif nums[mid - 1] > nums[mid]:
-                 return nums[mid]
-             else:
-                 break
+        # base case len(nums) == 2
+        while l < r:
+            mid = (l + r) // 2
+            # max edge
+            if nums[mid + 1] < nums[mid]:
+                return nums[mid + 1]
+            # min edge
+            elif nums[mid - 1] > nums[mid]:
+                return nums[mid]
+            # mid < r means lesser value numbers exist to the left
+            elif nums[mid] < nums[r]:
+                r = mid - 1
+            # mid > r means we are in a rotated portion of the array and a minimum value exists to the right
+            else:
+                l = mid + 1
+                # mid == r should be impossible
+        # only one element in the list
+        return nums[0]
