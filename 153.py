fc3e3e1246703e15ccc4ecf30d5a7135751f558a
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-10 16:27:21 -0500

    Initial code can find min if we're at one of the edges of the list before rotation

--- 153.py
+++ 153.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        l, r = 0, len(nums) - 1
+        while l <= r:
+             mid = (l + r) // 2
+             if nums[mid + 1] < nums[mid]:
+                 return nums[mid + 1]
+             elif nums[mid - 1] > nums[mid]:
+                 return nums[mid]
+             else:
+                 break
