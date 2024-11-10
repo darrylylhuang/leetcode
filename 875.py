d94f81ecdd64a23743600847869f5052ca5a75f0
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-10 13:49:28 -0500

    Set `l`, `r` pointers

--- 875.py
+++ 875.py
@@ -5,3 +5,7 @@ class Solution(object):
         :type h: int
         :rtype: int
         """
+        # if we have a very generous h, we can eat one banana at a time
+        # having a banana-eating rate higher than our largest pile is pointless
+        l, r = 1, max(piles)
+        mid = (l + r) // 2
