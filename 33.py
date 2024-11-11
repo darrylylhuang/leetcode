a69f7f2b85d280394381d2a6f3f8b8557fb4c1ff
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-10 21:20:50 -0500

    Some initial `if` case ideas, but they are not correctly accounting for rotation

--- 33.py
+++ 33.py
@@ -5,3 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        l, r = 0, len(nums) - 1
+        # base case len(nums) == 2
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+            elif target == nums[r]:
+                return r
+            # target between mid and r sorted
+            # hope the list has been rotated and check right
+            elif target > nums[mid] and target < nums[r] or target < nums[mid] and target > nums[r]:
+                l = mid + 1
+            # target between l and mid sorted
+            # hope the list has been rotated and check left
+            elif target < nums[mid] and target > nums[r] or target > nums[mid] and target < nums[r]:
+                r = mid - 1
+            else:
+                return -1
+        return -1
