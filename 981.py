9eb830d4ee7feece2c0e70f5ba156090d23fad20
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-12 14:00:58 -0500

    Replaced `._values` with `._store` to differentiate with the `values` list

--- 981.py
+++ 981.py
@@ -1,7 +1,7 @@
 class TimeMap(object):
 
     def __init__(self):
-        self._values = dict()
+        self._store = dict()
 
     def set(self, key, value, timestamp):
         """
@@ -10,11 +10,11 @@ class TimeMap(object):
         :type timestamp: int
         :rtype: None
         """
-        if key in self._values:
-            self._values[key][0].append(timestamp)
-            self._values[key][1].append(value)
+        if key in self._store:
+            self._store[key][0].append(timestamp)
+            self._store[key][1].append(value)
         else:
-            self._values[key] = [[timestamp], [value]]
+            self._store[key] = [[timestamp], [value]]
 
     def get(self, key, timestamp):
         """
@@ -22,12 +22,12 @@ class TimeMap(object):
         :type timestamp: int
         :rtype: str
         """
-        if not key in self._values:
+        if not key in self._store:
             return ""
 
         # the timestamp for the last element in the list will be the max since timestamp is strictly increasing
-        old_timestamps = self._values[key][0]
-        values = self._values[key][1]
+        old_timestamps = self._store[key][0]
+        values = self._store[key][1]
 
         # max old_timestamp is less than given so we return
         if old_timestamps[-1] <= timestamp:
