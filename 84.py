7d4622d0e82259f7a7839208c3f5ebb375db11e0
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-14 17:23:25 -0500

    Solution

--- 84.py
+++ 84.py
@@ -6,8 +6,20 @@ class Solution(object):
         """
         max_area = 0
         stack = []
-        for height, i in enumerate(heights):
+        for i, height in enumerate(heights):
             if not stack or height > stack[-1][1]:
                 stack.append((i, height))
+            else:
+                while stack and stack[-1][1] > height:
+                    dimensions = stack.pop()
+                    max_area = max(
+                        max_area, (i - dimensions[0]) * dimensions[1])
+                stack.append((dimensions[0], height))
+
+        i += 1
+        while stack:
+            dimensions = stack.pop()
+            max_area = max(
+                max_area, (i - dimensions[0]) * dimensions[1])
 
         return max_area
