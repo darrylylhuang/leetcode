3e5d61550cd7a2f328f8dee26f8d1212e2f6212d
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-14 15:03:33 -0500

    Calculate the intermediate wide area constrained by previous min upon reaching a new min.

--- 84.py
+++ 84.py
@@ -6,3 +6,11 @@ class Solution(object):
         """
         # 1 <= heights.length <= 10^5
         max_area = heights[0]
+        stack = [max_area]
+        for height, i in enumerate(heights):
+            if height < stack[-1]:
+                # intermediate wide area constrained by previous stack min = i * stack[-1]
+                max_area = max(max_area, i * stack[-1])
+                stack.append(height)
+
+        return max_area
