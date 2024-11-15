b6d7d73807be70f8024ba1a0833e56e6b9536950
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-15 11:13:16 -0500

    Comments for pointer movement

--- 4.py
+++ 4.py
@@ -7,12 +7,15 @@ class Solution(object):
         """
         l1, r1 = 0, len(nums1) - 1
         l2, r2 = 0, len(nums2) - 1
+        # pointers in different lists may move at different rates
 
+        # move the lesser left pointer towards the "middle"
         if nums1[l1] < nums2[l2]:
             l1 = (l1 + r1) // 2
         else:
             l2 = (l2 + r2) // 2
 
+        # move the greater right pointer towards the "middle"
         if nums1[r1] < nums2[r2]:
             r2 = (l2 + r2) // 2
         else:
