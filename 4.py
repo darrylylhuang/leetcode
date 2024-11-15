3b88a9f0f9438c8d0966a4b98b1f81e742347a8a
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-15 11:11:52 -0500

    Move the lesser left pointer and the greater right pointer towards the middle

--- 4.py
+++ 4.py
@@ -7,3 +7,13 @@ class Solution(object):
         """
         l1, r1 = 0, len(nums1) - 1
         l2, r2 = 0, len(nums2) - 1
+
+        if nums1[l1] < nums2[l2]:
+            l1 = (l1 + r1) // 2
+        else:
+            l2 = (l2 + r2) // 2
+
+        if nums1[r1] < nums2[r2]:
+            r2 = (l2 + r2) // 2
+        else:
+            r1 = (l1 + r1) // 2
