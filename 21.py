c07be5734a4d0d8e9eef4f8aa0dbae75a79d6248
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-15 01:15:37 -0500

    Initial values; appending `next` values to new list

--- 21.py
+++ 21.py
@@ -11,17 +11,40 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         if list1 is None:
-            head = list2
+            return list2
         elif list2 is None:
-            head = list1
+            return list1
         elif list1.val < list2.val:
             head = list1
+            curr1 = head.next
+            curr2 = list2
         else:
             head = list2
+            curr1 = list1
+            curr2 = head.next
 
         curr = head
-
         while curr:
-            break
+            # both lists have been exhausted
+            if curr1 is None and curr2 is None:
+                # we can point to the end
+                curr.next = None
+            # list1 has been exhausted
+            elif curr1 is None:
+                # append the rest of list2 and break
+                curr.next = curr2
+                break
+            # list2 has been exhausted
+            elif curr2 is None:
+                # append the rest of list1 and break
+                curr.next = curr1
+                break
+            elif curr1 < curr2:
+                curr.next = curr1
+                curr1 = curr1.next
+            else:
+                curr.next = curr2
+                curr2 = curr2.next
+            curr = curr.next
 
         return head
