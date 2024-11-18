a0bb1f4aa40c9d879624b48af05f8afbe5afba9f
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-18 10:59:58 -0500

    Solution

--- 226.py
+++ 226.py
@@ -10,3 +10,22 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: Optional[TreeNode]
         """
+        return self.invertTreeR(root)
+
+    def invertTreeR(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return root
+
+        # invert children
+        self.invertTreeR(root.left)
+        self.invertTreeR(root.right)
+
+        # swap children so that this node is inverted
+        placeholder = root.left
+        root.left = root.right
+        root.right = placeholder
+        return root
