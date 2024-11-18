be11f60aa5ff35e03c71fa91e3d6d1ecd0c1dfda
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-18 15:24:56 -0500

    Solution 2 with internal function and member variable.

--- 543.py
+++ 543.py
@@ -10,13 +10,14 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        return self.height_and_max_diameter(root)[1]
+        # return self.height_and_max_diameter(root)[1]
+        return self.diameterOfBinaryTree2(root)
 
     def height_and_max_diameter(self, root):
         """
         Find height and max diameter recursively
         :type root: Optional[TreeNode]
-        :rtype: int
+        :rtype: (int, int)
         """
         # Null nodes are depth 0, and have diameter 0
         if not root:
@@ -29,3 +30,24 @@ class Solution(object):
             diameter = l_height + r_height
             max_diameter = max(diameter, l_diameter, r_diameter)
             return (height, max_diameter)
+
+    def diameterOfBinaryTree2(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        self.max_diameter = 0
+
+        def calculate_height_update_max_diameter(tree):
+            """
+            Returns height and updates max diameter
+            """
+            if not tree:
+                return 0
+            l = calculate_height_update_max_diameter(tree.left)
+            r = calculate_height_update_max_diameter(tree.right)
+            self.max_diameter = max(self.max_diameter, l + r)
+            return max(l, r) + 1
+
+        calculate_height_update_max_diameter(root)
+        return self.max_diameter
