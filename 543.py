5c2752bab21726054dcee1ee63aa5b2f34ccb041
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-18 14:49:05 -0500

    Initial solution

--- 543.py
+++ 543.py
@@ -10,3 +10,28 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
+        return self.diameterOfBinaryTreeR(root)
+
+    def diameterOfBinaryTreeR(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+        # diameter = left height + right height
+        # children may have larger sums than root if the tree is unbalanced
+        return max(self.maxDepthR(root.left) + self.maxDepthR(root.right), self.diameterOfBinaryTreeR(root.left), self.diameterOfBinaryTreeR(root.right))
+
+    def maxDepthR(self, root):
+        """
+        Find max depth recursively
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        # Null nodes are depth 0
+        if not root:
+            return 0
+        # Check children's heights and add 1 for the current node
+        else:
+            return max(self.maxDepthR(root.left), self.maxDepthR(root.right)) + 1
