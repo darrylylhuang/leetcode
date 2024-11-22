cf3cc704f7024b4de52bab5d3189f771423e533d
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-22 15:36:13 -0500

    `new.next` may already exist from `old.random`; handle both cases

--- 138.py
+++ 138.py
@@ -23,10 +23,16 @@ class Solution(object):
         old_to_new = dict()
         old_to_new[old] = new
         while new:
-            if old.next is None:
-                new.next = None
+            # there is a chance that the next Node already exists because of random
+            if old.next in old_to_new:
+                new.next = old_to_new[old.next]
+            # if the next Node doesn't exist, create and map it even if it is None
             else:
-                new.next = Node(old.next)
+                if old.next is None:
+                    new.next = None
+                else:
+                    new.next = Node(old.next.val)
+                old_to_new[old.next] = new.next
 
             new = new.next
             old = old.next
