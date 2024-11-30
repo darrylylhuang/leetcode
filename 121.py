0e432191bedb24b0f1cbf5ee82200171e1351ffd
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-11-30 15:03:49 -0500

    Solution

--- 121.py
+++ 121.py
@@ -4,3 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        max_profit = 0
+        l = 0
+        for r in range(len(prices)):
+            max_profit = max(max_profit, prices[r] - prices[l])
+            if prices[r] < prices[l]:
+                l = r
+        return max_profit
