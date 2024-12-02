3d9d6cca21e87a57ab310635b3050fbb649edc57
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-12-02 18:46:59 -0500

    Solution for ADJACENT duplicate letters (should be NO duplicates)

--- 3.py
+++ 3.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        max_length = 0
+        start = 0
+        for i in range(1, len(s)):
+            # duplicate letter
+            if s[i] == s[i - 1]:
+                # possibly new max length
+                max_length = max(max_length, i - start)
+                # move start of substring pointer
+                start = i
+
+        # final check (substring may run to the end of the string)
+        max_length = max(max_length, i - start)
+        return max_length
