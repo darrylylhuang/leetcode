1ff1b04f309c77412b66aa6df5ab28962d44b4bc
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-12-02 20:05:52 -0500

    NeetCode solution uses a `set()`

--- 3.py
+++ 3.py
@@ -4,6 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        return self.lengthOfLongestSubstringDict(s)
+
+    def lengthOfLongestSubstringDict(self, s):
+        """
+        Uses a dictionary of indices to move the left pointer past the duplicate letter
+        :type s: str
+        :rtype: int
+        """
         max_length = 0
         # initialize r = -1 for empty string
         l, r = 0, -1
@@ -20,4 +28,26 @@ class Solution(object):
         # final check (substring may run to the end of the string)
         # remember to move the right pointer forward
         max_length = max(max_length, r + 1 - l)
+        return max_length
+
+    def lengthOfLongestSubstringSet(self, s):
+        """
+        Uses a set and increments left pointer to remove letters up to the duplicate
+        :type s: str
+        :rtype: int
+        """
+        max_length = 0
+        # maintain sliding window with left/right pointers
+        l = 0
+        # maintain characters seen to prevent duplicates
+        seen = set()
+        for r in range(len(s)):
+            while s[r] in seen:
+                # increment left and remove letters from the set until we've reached the duplicate
+                seen.remove(s[l])
+                l += 1
+            # index math, but we're counting characters: add 1
+            max_length = max(max_length, r - l + 1)
+            seen.add(s[r])
+
         return max_length
