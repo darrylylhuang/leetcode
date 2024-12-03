38ab148c1f9d4f1c10b9a82dbd57bdbe125f9267
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-12-03 13:19:38 -0500

    Calculate substring length and determine validity based on most frequent character; move sliding window if not

--- 424.py
+++ 424.py
@@ -5,10 +5,22 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        max_length = 0
         l = 0
         char_count = dict()
         for r in range(len(s)):
             if s[r] in char_count:
-                s[r] += 1
+                char_count[r] += 1
             else:
-                s[r] = 1
+                char_count[r] = 1
+
+            substring_length = r - l + 1
+            # a valid substring of duplicate characters can be created by substituting k letters
+            if substring_length - max(char_count.values()) <= k:
+                # potentially update max length
+                max_length = max(max_length, substring_length)
+            else:
+                # move the sliding window
+                l += 1
+                char_count[l] -= 1
+        return max_length
