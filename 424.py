34a0073d1a457d3d38b267109c683ca954568fd4
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-12-03 13:06:16 -0500

    Initialize sliding window and character count

--- 424.py
+++ 424.py
@@ -5,3 +5,10 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        l = 0
+        char_count = dict()
+        for r in range(len(s)):
+            if s[r] in char_count:
+                s[r] += 1
+            else:
+                s[r] = 1
