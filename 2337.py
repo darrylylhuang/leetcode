e6656016a083a97db56558271f50a2449cc2a6f2
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-12-05 19:43:47 -0500

    Validity checks should happen on the corresponding piece we're handling

--- 2337.py
+++ 2337.py
@@ -18,36 +18,42 @@ class Solution(object):
         # handle L
         obstruction = -1
         for i, char in enumerate(end):
-            if char == 'R':
+            if char == '_':
+                continue
+            elif char == 'R':
                 obstruction = i
-            # the L piece in start is further left than the L piece in target
-            elif char == 'L' and i < L_positions[0]:
-                return False
-            # something is blocking the L piece in start from moving to its correct position in target
-            elif L_positions[0] < obstruction:
-                return False
-            # L is unobstructed from moving to its correct position in target
-            else:
-                end[L_positions[0]] = "L"
-                end[i] = "_"
-                L_positions.pop(0)
+            elif char == 'L':
+                # the L piece in start is further left than the L piece in target
+                if i < L_positions[0]:
+                    return False
+                # something is blocking the L piece in start from moving to its correct position in target
+                elif L_positions[0] < obstruction:
+                    return False
+                # L is unobstructed from moving to its correct position in target
+                else:
+                    end[L_positions[0]] = "L"
+                    end[i] = "_"
+                    L_positions.pop(0)
 
         # handle R
         obstruction = len(target)
         for i in range(len(target) - 1, -1, -1):
             char = end[i]
+            if char == '_':
+                continue
             if char == 'L':
                 obstruction = i
             # the R piece in start is further right than the R piece in target
-            elif char == 'R' and i > R_positions[-1]:
-                return False
-            # something is blocking the R piece in start from moving to its correct position in target
-            elif obstruction < R_positions[-1]:
-                return False
-            # R is unobstructed from moving to its correct position in target
-            else:
-                end[R_positions[-1]] = "R"
-                end[i] = "_"
-                L_positions.pop()
+            elif char == 'R':
+                if i > R_positions[-1]:
+                    return False
+                # something is blocking the R piece in start from moving to its correct position in target
+                elif obstruction < R_positions[-1]:
+                    return False
+                # R is unobstructed from moving to its correct position in target
+                else:
+                    end[R_positions[-1]] = "R"
+                    end[i] = "_"
+                    L_positions.pop()
 
         return True
