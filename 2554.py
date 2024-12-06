52d47a407f7e2de44ccc4efb0cbc8817e25766ef
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-12-06 17:03:14 -0500

    Starter code

---
+++ 2554.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maxCount(self, banned, n, maxSum):
+        """
+        :type banned: List[int]
+        :type n: int
+        :type maxSum: int
+        :rtype: int
+        """
