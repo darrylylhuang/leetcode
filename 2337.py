30dcf65a3f4d316a2304d5f5ca2934a8b3f33779
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-12-05 19:11:12 -0500

    Move "L" pieces from `start` to their correct position in `target`

--- 2337.py
+++ 2337.py
@@ -12,3 +12,22 @@ class Solution(object):
                 L_positions.append(i)
             if char == 'R':
                 R_positions.append(i)
+
+        end = [char for char in start]
+
+        # handle L
+        obstruction = -1
+        for i, char in enumerate(end):
+            if char == 'R':
+                obstruction = i
+            # the L piece in start is further left than the L piece in target
+            elif char == 'L' and i < L_positions[0]:
+                return False
+            # something is blocking the L piece in start from moving to its correct position in target
+            elif L_positions[0] < obstruction:
+                return False
+            # L is unobstructed from moving to its correct position in target
+            else:
+                end[L_positions[0]] = "L"
+                end[i] = "_"
+                L_positions.pop(0)
