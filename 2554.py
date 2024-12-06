8fa3bccf7420699799a74fcace9c4443a158ac0a
darrylylhuang <darryl.thatguy.13@gmail.com>
2024-12-06 17:12:39 -0500

    Brute force solution `O(n)`

--- 2554.py
+++ 2554.py
@@ -6,3 +6,15 @@ class Solution(object):
         :type maxSum: int
         :rtype: int
         """
+        sum = 0
+        ints = 0
+        for i in range(1, n + 1):
+            if i in banned:
+                continue
+
+            sum += i
+            if sum > maxSum:
+                return ints
+
+            ints += 1
+        return ints
